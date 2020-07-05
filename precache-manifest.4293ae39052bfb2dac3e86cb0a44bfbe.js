84529c201be9c8a8b5e07236bc563c952b74dac4
nileshvarishe <nileshvarishe@gmail.com>
2020-07-05 14:46:45 +0530

    Updates

---
+++ precache-manifest.4293ae39052bfb2dac3e86cb0a44bfbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fcc101c355c3fe7d8b8659e281a9a5d",
+    "url": "/edkal/index.html"
+  },
+  {
+    "revision": "05b62a92a3e5128c17c5",
+    "url": "/edkal/static/css/main.fcc39c7c.chunk.css"
+  },
+  {
+    "revision": "9a023e246806f68a202e",
+    "url": "/edkal/static/js/2.daec6fe3.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/edkal/static/js/2.daec6fe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05b62a92a3e5128c17c5",
+    "url": "/edkal/static/js/main.224b1424.chunk.js"
+  },
+  {
+    "revision": "073ce066c79ba9a31c62",
+    "url": "/edkal/static/js/runtime-main.e266e31b.js"
+  }
+]);
